160d25310ebdb33714e08a2271fdae9a5655cdf6
Dubemsky <122183811+Dubemsky@users.noreply.github.com>
2023-08-08 15:18:28 +0100

    Add files via upload

---
+++ Cook/bookings.php
@@ -0,0 +1,4 @@
+<?php
+echo "dubem is a good";
+
+?>
---
+++ Cook/homepage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Login</h2>
+        <form action="#" method="#">
+
+            <div class="input-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+
+            <div class="input-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit" class="btn">
+                <a href="bookings.php">Go to Bookings</a>
+            </button>
+        </form>
+    </div>
+</body>
+</html>
